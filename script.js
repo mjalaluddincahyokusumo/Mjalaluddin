9e546bd439b715e4523f519f78e99ea83d0360a5
mjalaluddincahyokusumo <muhammad.jalaluddin260@smk.belajar.id>
2024-11-13 08:37:08 +0700

    Done

---
+++ script.js
@@ -0,0 +1,9 @@
+document.querySelectorA11('a[href^="#]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+})
